d24d0a35563c3d9589453d8411ed552023cd7e69
Jesseh527 <Jesseh52700@gmail.com>
2024-10-02 13:48:49 -0700

    update for website title

--- UI/app.py
+++ UI/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 # Function for chatbot responses
-st.set_page_config(page_title = "Academic Chatbot - Team2", page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title = "Academic Chatbot - Team2")
 
 def chatbot_response(user_input):
     responses = {
